4d5c45299581cb62ac0da6139bf7dd33102323c7
tanim-k <tanimkhan877@gmail.com>
2022-02-28 20:32:07 +0600

    api data load

--- js/post.js
+++ js/post.js
@@ -3,7 +3,32 @@ function loadPosts(){
     .then(res => res.json())
     .then(data => displayPosts(data))
 }
-loadPosts();
+// loadPosts();
 function displayPosts(posts) {
-    console.log(posts);
+    const postDiv = document.getElementById('posts');
+    for( const post of posts ) {
+        const div = document.createElement('div');
+        div.classList.add('post')
+        div.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p> `;
+        postDiv.appendChild(div);
+        console.log(post.body)
+    }
+}
+function addAPost() {
+    fetch('https://jsonplaceholder.typicode.com/posts/', {
+        method: 'POST',
+        body: JSON.stringify({
+            title:'my new post',
+            body: 'this is my post',
+            userId: 1,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+          },
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
+
 }
